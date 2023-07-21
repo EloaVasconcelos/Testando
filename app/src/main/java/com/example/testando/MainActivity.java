46a78da785cf41079835a8193aaaca49335abc27
EloaVasconcelos <127448572+EloaVasconcelos@users.noreply.github.com>
2023-07-21 20:14:40 -0300

    Segundo commit

--- app/src/main/java/com/example/testando/MainActivity.java
+++ app/src/main/java/com/example/testando/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.testando;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnteste;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
